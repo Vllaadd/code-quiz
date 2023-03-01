158cc7d56ea1463ef5fa66f6eb2dd3e50836bd77
Vllaadd <vlladdiimiir@gmail.com>
2023-03-01 02:00:51 -0500

    real questions and answers added

--- questions.js
+++ questions.js
@@ -1,32 +1,54 @@
 const questions = [
     {
         question: "What does HTML stand for?",
-        answers: {11 : "Something Something ", 
-                12 : "SOmething something so",
-                13 : "kjfdg kjef",
-                14 : "somfh fis frtr"},
-        correctAnswer: 14
+        answers: 
+            {
+                11 : "Hyper Transient Maker Language",
+                12 : "Hyper Text Markup Language",
+                13 : "Halode Typographic Maximized Lexicon",
+                14 : "Hadron Transforming Markdown Launcher"
+            },
+        correctAnswer: 12
 
     },{
-        question: "What does HTML stand for?",
-        answers: {11 : "Something Something ", 
-                12 : "SOmething something so",
-                13 : "kjfdg kjef",
-                14 : "somfh fis frtr"},
-        correctAnswer: 14 
+        question: "The following is not an array method: ",
+        answers: 
+        {   
+            21 : "array.splice()", 
+            22 : "array.slice()",
+            23 : "array.spin()",
+            24 : "array.split()"
+        },
+        correctAnswer: 23 
     },{
-        question: "What does HTML stand for?",
-        answers: {11 : "Something Something ", 
-                12 : "SOmething something so",
-                13 : "kjfdg kjef",
-                14 : "somfh fis frtr"},
-        correctAnswer: 14
+        question: "Arrays in JavaScript can be used to store___.?",
+        answers: 
+        {
+            31 : "Numbers and strings", 
+            32 : "Other arrays",
+            33 : "Booleans",
+            34 : "All of the above"
+        },
+        correctAnswer: 34
     },{
-        question: "What does HTML stand for?",
-        answers: {11 : "Something Something ", 
-                12 : "SOmething something so",
-                13 : "kjfdg kjef",
-                14 : "somfh fis frtr"},
-        correctAnswer: 14
+        question: "String values must be enclosed within___when being assigned to variables.?",
+        answers: 
+        {
+            41 : "Commas", 
+            42 : "Curly brackets",
+            43 : "Quotes",
+            44 : "Parentheses"
+        },
+        correctAnswer: 43
+    },{
+        question: "If (i != 0) runs when",
+        answers: 
+        {
+            51 : "i is not equal to 0", 
+            52 : "i is equal to 0",
+            53 : "i = zero",
+            54 : "i is undefined"
+        },
+        correctAnswer: 51
     }
 ];
