9b75480315b19bd116d063093eb0846f5a4ec6e7
Vllaadd <vlladdiimiir@gmail.com>
2023-03-01 01:49:38 -0500

    dummy questions created

---
+++ questions.js
@@ -0,0 +1,32 @@
+const questions = [
+    {
+        question: "What does HTML stand for?",
+        answers: {11 : "Something Something ", 
+                12 : "SOmething something so",
+                13 : "kjfdg kjef",
+                14 : "somfh fis frtr"},
+        correctAnswer: 14
+
+    },{
+        question: "What does HTML stand for?",
+        answers: {11 : "Something Something ", 
+                12 : "SOmething something so",
+                13 : "kjfdg kjef",
+                14 : "somfh fis frtr"},
+        correctAnswer: 14 
+    },{
+        question: "What does HTML stand for?",
+        answers: {11 : "Something Something ", 
+                12 : "SOmething something so",
+                13 : "kjfdg kjef",
+                14 : "somfh fis frtr"},
+        correctAnswer: 14
+    },{
+        question: "What does HTML stand for?",
+        answers: {11 : "Something Something ", 
+                12 : "SOmething something so",
+                13 : "kjfdg kjef",
+                14 : "somfh fis frtr"},
+        correctAnswer: 14
+    }
+];
