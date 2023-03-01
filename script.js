c76df192ff3ef4440ccb1bb2d2408a4bea0d0f98
Vllaadd <vlladdiimiir@gmail.com>
2023-03-01 00:47:06 -0500

    time and code show up after click

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    $("button").click(function(){
+       $("#emptyTime").append("<h5>Time:<span></span></h5>");
+       $("#emptyScore").append("<h5>Score:<span></span></h5>");
+
+    })
+ 
+
+
+});
+
+
+//once we press the button first Question with answers show up 
+
+//once we press the button Time starts running 
