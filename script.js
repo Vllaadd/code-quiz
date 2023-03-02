9aaf1153e85e19a0f922bb0adaf5286746051f43
Vllaadd <vlladdiimiir@gmail.com>
2023-03-02 02:39:47 -0500

    click event showElements countdown all working

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $("button").click(function(){
-       $("#emptyTime").append("<h5>Time:<span></span></h5>");
-       $("#emptyScore").append("<h5>Score:<span></span></h5>");
 
-    })
- 
+    $("button").click(timerCountdown);
+
+    function timerCountdown() {
+        showElements();
+        let seconds = 5;
+        const intervAl = setInterval(()=>{
+            seconds--;
+            console.log(seconds);
+            $("#emptyTime").html(seconds);
+            if(seconds<0){
+                clearInterval(intervAl);
+                $("#emptyTime").html("That's all folks!");
+            };
+        },1000);
+    }
+
+    function showElements() {
+        $("#emptyTime").append("<h5>Time:<span id='current-time'></span></h5>");
+        $("#emptyScore").append("<h5>Score:<span></span></h5>");
+    }
 
 
 });
-
-
-//once we press the button first Question with answers show up 
-
-//once we press the button Time starts running 
+ 
