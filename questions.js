d4d7acad942cd2a6d237666975f8ff64283fa8fc
Vllaadd <vlladdiimiir@gmail.com>
2023-03-04 12:01:49 -0500

    displaying elements on page

--- questions.js
+++ questions.js
@@ -3,10 +3,10 @@ const questions = [
         question: "What does HTML stand for?",
         answers: 
             {
-                11 : "Hyper Transient Maker Language",
-                12 : "Hyper Text Markup Language",
-                13 : "Halode Typographic Maximized Lexicon",
-                14 : "Hadron Transforming Markdown Launcher"
+                11 : "1. Hyper Transient Maker Language",
+                12 : "2. Hyper Text Markup Language",
+                13 : "3. Halode Typographic Maximized Lexicon",
+                14 : "4. Hadron Transforming Markdown Launcher"
             },
         correctAnswer: 12
 
@@ -14,40 +14,40 @@ const questions = [
         question: "The following is not an array method: ",
         answers: 
         {   
-            21 : "array.splice()", 
-            22 : "array.slice()",
-            23 : "array.spin()",
-            24 : "array.split()"
+            21 : "1. array.splice()", 
+            22 : "2. array.slice()",
+            23 : "3. array.spin()",
+            24 : "4. array.split()"
         },
         correctAnswer: 23 
     },{
         question: "Arrays in JavaScript can be used to store___.?",
         answers: 
         {
-            31 : "Numbers and strings", 
-            32 : "Other arrays",
-            33 : "Booleans",
-            34 : "All of the above"
+            31 : "1. Numbers and strings", 
+            32 : "2. Other arrays",
+            33 : "3. Booleans",
+            34 : "4. All of the above"
         },
         correctAnswer: 34
     },{
         question: "String values must be enclosed within___when being assigned to variables.?",
         answers: 
         {
-            41 : "Commas", 
-            42 : "Curly brackets",
-            43 : "Quotes",
-            44 : "Parentheses"
+            41 : "1. Commas", 
+            42 : "2. Curly brackets",
+            43 : "3. Quotes",
+            44 : "4. Parentheses"
         },
         correctAnswer: 43
     },{
         question: "If (i != 0) runs when",
         answers: 
         {
-            51 : "i is not equal to 0", 
-            52 : "i is equal to 0",
-            53 : "i = zero",
-            54 : "i is undefined"
+            51 : "1. i is not equal to 0", 
+            52 : "2. i is equal to 0",
+            53 : "3. i = zero",
+            54 : "4. i is undefined"
         },
         correctAnswer: 51
     }
--- script.js
+++ script.js
@@ -11,26 +11,29 @@ $(document).ready(function () {
         const intervAl = setInterval(()=>{
             seconds--;
             console.log(seconds);
-            $("#emptyTime").html("Time: " + seconds);
+            $("#timeDiv").html("Time: " + seconds);
             if(seconds<0){
                 clearInterval(intervAl);
-                $("#emptyTime").html("Time Is Up!");
+                $("#timeDiv").html("Time Is Up!");
             };
         },1000);
     }
 
 // show score on page
     function showScore(){
-        $("#emptyScore").append("<h5>Score:<span></span></h5>");
+        $("#scoreDiv").append("<h5>Score:<span></span></h5>");
     }
 
     function startQuiz(){
-        $("#startBttn").remove();
-        let questionOne = $("<button></button>");
-        let questionOneString = JSON.stringify(questions);
-        let questionOneParse = JSON.parse(questionOneString);
-        questionOneParse.text(questionOneString[1][1]);
-        $(".quizSpace").append(questionOne);
+        questions.forEach(function(question){
+            $("#startBttn").remove();
+        let questionDiv = $("<button></button>").text(question.answers[11] ++)
+        // .text(question.answers[12]).text(question.answers[13]).text(question.answers[14]).text(question.answers[15]);
+        // console.log(question.answers)
+        // questionOne.text(question.answers[21]);
+        $(".quizSpace").append(questionDiv);
+        })
+        
         // let questionOne = $("<button></button>").text("Question 1");
         // let questionTwo = $("<button></button>").text("Question 2");
         // let questionThree = $("<button></button>").text("Question 3");
