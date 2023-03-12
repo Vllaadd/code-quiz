d85bde2ee6adf86cae3663fe631cc488c9448858
Vllaadd <vlladdiimiir@gmail.com>
2023-03-12 12:15:38 -0400

    first question with answers displayed

--- script.js
+++ script.js
@@ -26,33 +26,13 @@ $(document).ready(function () {
 
     function questionOne(){
         $("#startBttn").remove();
-        questionTwo();
-        $(".quiz-space").append("<button id='questionOneBttn' onclick'questionTwo'>Question Two</button>");
-       for(let i=0; i<questions.length; i++){
-        console.log(questions["question"], questions[0].answers["11"], questions[0].answers["12"], questions[0].answers["13"], questions[0].answers["14"]);
-       }
-    }
-    function questionTwo(){
-        $("#questionOneBttn").remove();
-        questionThree();
-        $(".quiz-space").append("<button id='questionTwoBttn' onclick'questionThree'>Question Two</button>");
-       for(let i=0; i<questions.length; i++){
-        console.log(questions["question"], questions[1].answers["21"], questions[1].answers["22"], questions[1].answers["23"], questions[1].answers["24"]);
-       }
-    }
-    function questionThree(){
-        $("#questionTwoBttn").remove();
-        questionFour();
-        $(".quiz-space").append("<button id='questionThreeBttn' onclick'questionFour'>Question Two</button>");
-       for(let i=0; i<questions.length; i++){
-        console.log(questions["question"], questions[2].answers["31"], questions[2].answers["32"], questions[2].answers["33"], questions[2].answers["34"]);
-       }
-    }
-    function questionFour(){
-        $("#questionThreeBttn").remove();
-        $(".quiz-space").append("<button id='questionFourBttn' onclick'questionFour'>Question Two</button>");
-       for(let i=0; i<questions.length; i++){
-        console.log(questions["question"], questions[3].answers["41"], questions[3].answers["41"], questions[3].answers["43"], questions[3].answers["44"]);
-       }
-    }
+        console.log(questions[0]["answers"]["12"]);
+        $(".quiz-space").append("<button>" + questions[0]["question"] + "</button><br/>");
+        $(".quiz-space").append("<button>" + questions[0]["answers"]["11"] + "</button>");
+        $(".quiz-space").append("<button>" + questions[0]["answers"]["12"] + "</button>");
+        $(".quiz-space").append("<button>" + questions[0]["answers"]["13"] + "</button>");
+        $(".quiz-space").append("<button>" + questions[0]["answers"]["14"] + "</button><br/><br/>");
+        $(".quiz-space").append("<button>Submit</button>");
+      }
+    
 });
