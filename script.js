b5f8b652eaf9d389fd13c3626f59fe09f3080ffd
Vllaadd <vlladdiimiir@gmail.com>
2023-04-11 05:39:18 -0400

    it is not accessing the answers properly

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-
-    $("#start-button").click(function(){
+//home page - start button•••••••••••••••••••••••••••••••••••••••••••••••
+    $("#start-button").click(function () {
         showQuestionOne();
     });
 
-//question one function
-    function showQuestionOne(){
+    //question one••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+    function showQuestionOne() {
         $("#start-button").remove();
         $(".quiz-space").append("<button>" + questions[0]["question"] + "</button><br/>");
         $(".quiz-space").append("<button>" + questions[0]["answers"]["11"] + "</button>");
@@ -16,13 +16,25 @@ $(document).ready(function () {
             id: "question-two",
             text: "Go to question two"
         }).appendTo(".quiz-space");
-        $("#question-two").click(function(){
+        $("#question-two").click(function () {
             showQuestionTwo();
-           });
+        });
     };
 
-
-   function showQuestionTwo(){
-    console.log("Hi, I am finally working!");
-   };
+//question two••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••• 
+    function showQuestionTwo() {
+        $("#question-two").remove();
+        $(".quiz-space").append("<button>" + questions[1]["question"] + "</button><br/>");
+        $(".quiz-space").append("<button>" + questions[1]["answers"]["11"] + "</button>");
+        $(".quiz-space").append("<button>" + questions[1]["answers"]["12"] + "</button>");
+        $(".quiz-space").append("<button>" + questions[1]["answers"]["13"] + "</button>");
+        $(".quiz-space").append("<button>" + questions[1]["answers"]["14"] + "</button><br/><br/>");
+        $("<button>", {
+            id: "question-three",
+            text: "Go to question three"
+        }).appendTo(".quiz-space");
+        $("#question-three").click(function () {
+            showQuestionThree();
+        });
+    };
 });
