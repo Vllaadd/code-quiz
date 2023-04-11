b982467f64b7136d98ed795d8e365ea39f59055c
Vllaadd <vlladdiimiir@gmail.com>
2023-04-11 05:56:07 -0400

    iteration correct all data showing

--- script.js
+++ script.js
@@ -25,10 +25,10 @@ $(document).ready(function () {
     function showQuestionTwo() {
         $("#question-two").remove();
         $(".quiz-space").append("<button>" + questions[1]["question"] + "</button><br/>");
-        $(".quiz-space").append("<button>" + questions[1]["answers"]["11"] + "</button>");
-        $(".quiz-space").append("<button>" + questions[1]["answers"]["12"] + "</button>");
-        $(".quiz-space").append("<button>" + questions[1]["answers"]["13"] + "</button>");
-        $(".quiz-space").append("<button>" + questions[1]["answers"]["14"] + "</button><br/><br/>");
+        $(".quiz-space").append("<button>" + questions[1]["answers"]["21"] + "</button>");
+        $(".quiz-space").append("<button>" + questions[1]["answers"]["22"] + "</button>");
+        $(".quiz-space").append("<button>" + questions[1]["answers"]["23"] + "</button>");
+        $(".quiz-space").append("<button>" + questions[1]["answers"]["24"] + "</button><br/><br/>");
         $("<button>", {
             id: "question-three",
             text: "Go to question three"
@@ -40,11 +40,11 @@ $(document).ready(function () {
 
     function showQuestionThree() {
         $("#question-three").remove();
-        $(".quiz-space").append("<button>" + questions[1]["question"] + "</button><br/>");
-        $(".quiz-space").append("<button>" + questions[1]["answers"]["11"] + "</button>");
-        $(".quiz-space").append("<button>" + questions[1]["answers"]["12"] + "</button>");
-        $(".quiz-space").append("<button>" + questions[1]["answers"]["13"] + "</button>");
-        $(".quiz-space").append("<button>" + questions[1]["answers"]["14"] + "</button><br/><br/>");
+        $(".quiz-space").append("<button>" + questions[2]["question"] + "</button><br/>");
+        $(".quiz-space").append("<button>" + questions[2]["answers"]["31"] + "</button>");
+        $(".quiz-space").append("<button>" + questions[2]["answers"]["32"] + "</button>");
+        $(".quiz-space").append("<button>" + questions[2]["answers"]["33"] + "</button>");
+        $(".quiz-space").append("<button>" + questions[2]["answers"]["34"] + "</button><br/><br/>");
         $("<button>", {
             id: "question-four",
             text: "Go to question four"
@@ -54,8 +54,24 @@ $(document).ready(function () {
         });
     };
 
-        function showQuestionFour(){
-            console.log("Question four is working!")
-        }
+    function showQuestionFour() {
+        $("#question-three").remove();
+        $(".quiz-space").append("<button>" + questions[3]["question"] + "</button><br/>");
+        $(".quiz-space").append("<button>" + questions[3]["answers"]["41"] + "</button>");
+        $(".quiz-space").append("<button>" + questions[3]["answers"]["42"] + "</button>");
+        $(".quiz-space").append("<button>" + questions[3]["answers"]["43"] + "</button>");
+        $(".quiz-space").append("<button>" + questions[3]["answers"]["44"] + "</button><br/><br/>");
+        $("<button>", {
+            id: "score-button",
+            text: "See Your Score"
+        }).appendTo(".quiz-space");
+        $("#score-button").click(function () {
+            seeYourScore();
+        });
+    };
+
+    function seeYourScore(){
+        console.log("Congratulations! Your Score is One million Points")
+    }
 
 });
