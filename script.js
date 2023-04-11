0516aa3b72f6b629e16b116f7eed2c7e701a6bc9
Vllaadd <vlladdiimiir@gmail.com>
2023-04-11 02:34:01 -0400

    button two now shows up

--- script.js
+++ script.js
@@ -1,44 +1,33 @@
 $(document).ready(function () {
-
-
-    $("#start-button").click(questionOne);
-    $("#question-two").click(questionTwo);
-
-// click button to start the time and show questions 
-    function questionOne() {
+    $("#start-button").click(function(){
         showScore();
-        questionOne();
-        // let seconds = 5;
-        // const intervAl = setInterval(()=>{
-        //     seconds--;
-        //     console.log(seconds);
-        //     $("#timeDiv").html("Time: " + seconds);
-        //     if(seconds<0){
-        //         clearInterval(intervAl);
-        //         $("#timeDiv").html("Time Is Up!");
-        //     };
-        // },1000);
-    }
+        showQuestionOne();
+    });
 
 // show score on page
     function showScore(){
         $("#scoreDiv").append("<h5>Score:<span></span></h5>");
     }
 
-    function questionOne(){
+
+
+    function showQuestionOne(){
         $("#start-button").remove();
-        console.log(questions[0]["answers"]["12"]);
         $(".quiz-space").append("<button>" + questions[0]["question"] + "</button><br/>");
         $(".quiz-space").append("<button>" + questions[0]["answers"]["11"] + "</button>");
         $(".quiz-space").append("<button>" + questions[0]["answers"]["12"] + "</button>");
         $(".quiz-space").append("<button>" + questions[0]["answers"]["13"] + "</button>");
         $(".quiz-space").append("<button>" + questions[0]["answers"]["14"] + "</button><br/><br/>");
         $(".quiz-space").append("<button id='question-two'>Go to question two</button>");
+        $("#question-two").show();
       }
 
-    function questionTwo(){
-        console.log("Hi, I am finally working .... or not...");
-    }
+   $("#question-two").click(function(){
+    showQuestionTwo();
+   });
 
-    
+   function showQuestionTwo(){
+    console.log("Hi, I am finally working!");
+   };
+
 });
