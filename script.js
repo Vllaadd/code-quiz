dcdf7131df91a5f2071ebbbfc9b99f17e26138e7
Vllaadd <vlladdiimiir@gmail.com>
2023-04-11 04:51:04 -0400

    second question button still not working

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-    $("#question-two").hide();
 
     $("#start-button").click(function(){
         showQuestionOne();
@@ -12,8 +11,11 @@ $(document).ready(function () {
         $(".quiz-space").append("<button>" + questions[0]["answers"]["12"] + "</button>");
         $(".quiz-space").append("<button>" + questions[0]["answers"]["13"] + "</button>");
         $(".quiz-space").append("<button>" + questions[0]["answers"]["14"] + "</button><br/><br/>");
-        $(".quiz-space").append($("<button>").attr("id", "question-two").text("Go to question two").appendTo(".quiz-space"));
-      };
+        $("<button>", {
+            id: "question-two",
+            text: "Go to question two"
+        }).appendTo(".quiz-space");
+    };
 
 
     $("#question-two").click(function(){
