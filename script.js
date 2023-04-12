37476b352e2ce7c84e636ccf1fbb9b3ca6efd734
Vllaadd <vlladdiimiir@gmail.com>
2023-04-12 04:26:37 -0400

    minor fixes

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
         $("#start-button").remove();
         const cardEl = $("<div>").addClass("card").css({ "width": "18rem" });
         cardEl.append("<div>" + questions[0]["question"] + "</div>").addClass("card-header");
-        const ulEl = $("<ul>").addClass("list-group list-group-flush");
+        const ulEl = $("<ul>", {class: "list-group list-group-flush"});       
         const answerOne = $("<li>" + questions[0]["answers"]["11"] + "</li>").addClass("list-group-item");
         const answerTwo = $("<li>" + questions[0]["answers"]["12"] + "</li>").addClass("list-group-item");
         const answerThree = $("<li>" + questions[0]["answers"]["13"] + "</li>").addClass("list-group-item");
@@ -32,7 +32,7 @@ $(document).ready(function () {
         const cardEl = $("<div>").addClass("card").css({ "width": "18rem" });
         cardEl.append("<div>" + questions[1]["question"] + "</div>").addClass("card-header");
         const ulEl = $("<ul>").addClass("list-group list-group-flush");
-        const answerOne = $("<li>" + questions[1]["answers"]["21"] + "</li>").addClass("list-group-item");
+        const answerOne = $("<li>" + questions[1]["answers"]["21"] + "</li>").addClass("list-group-item ");
         const answerTwo = $("<li>" + questions[1]["answers"]["22"] + "</li>").addClass("list-group-item");
         const answerThree = $("<li>" + questions[1]["answers"]["23"] + "</li>").addClass("list-group-item");
         const answerFour = $("<li>" + questions[1]["answers"]["24"] + "</li>").addClass("list-group-item");
